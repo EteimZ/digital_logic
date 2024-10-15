7f4cda23625d16225431455b80c0570efdc4bcd3
Eteimz <8teims@gmail.com>
2024-10-15 04:52:24 +0100

    Added FullSubstractor and NBitSubstractor

--- digital/arithmetic.py
+++ digital/arithmetic.py
@@ -50,18 +50,30 @@ class NBitAdder:
     @staticmethod
     def logic(a: list[bool], b:list[bool], n=8):
 
-        result = []
+        if n <= 1:
+            raise ValueError("n cannot be less than or equal to one")
         
-        result1 = HalfAdder.logic(a[n-1], b[n-1])
+        if isinstance(a, list) == False or isinstance(b, list)  == False:
+            raise ValueError("Inputs should be a list")
         
-        result.append(result1["sum"])
+        if len(a) < n or len(b) < n:
+            raise ValueError("Input list cannot be less than n")
+        
+        full_result = []
+        
+        result = HalfAdder.logic(a[n-1], b[n-1])
+        
+        full_result.append(result["sum"])
         for i in reversed(range(n-1)):
-            resultl = FullAdder.logic(a[i], b[i], result1["carry"])
-            result.append(resultl["sum"])
+            result = FullAdder.logic(a[i], b[i], result["carry"])
+            full_result.append(result["sum"])
         
-        result.append(resultl["carry"])
-        result.reverse()
-        return result
+        # Sanitize result: ensure it is always 1s and 0s
+        result = int(bool(result["carry"]))
+        
+        full_result.append(result)
+        full_result.reverse()
+        return full_result
     
     def __call__(self, a, b, n):
         A = a if a != None else self.A
@@ -86,6 +98,64 @@ class HalfSubstractor:
         A = a if a != None else self.A
         B = b if b != None else self.B
         return self.logic(A, B)
+
+@dataclass
+class FullSubstractor:
+    A: bool = 0
+    B: bool = 0
+    C: bool = 0 
+
+    @staticmethod
+    def logic(a, b, c):
+        firstSubstractor = HalfSubstractor.logic(a, b)
+        SecondSubstractor = HalfSubstractor.logic(firstSubstractor["sub"], c)
+
+        orResult = OR.logic(firstSubstractor["remainder"], SecondSubstractor["remainder"])
+        return {"sub": SecondSubstractor["sub"], "remainder": orResult}
+
+    def __call__(self, a=None, b=None, c=None):
+        A = a if a != None else self.A
+        B = b if b != None else self.B
+        C = c if c != None else self.C
+        return self.logic(A, B, C)
+    
+@dataclass
+class NBitSubstractor:
+    A: list[bool]
+    B: list[bool]
+    n: int = 8 # Add case to prevent one bit
+
+    @staticmethod
+    def logic(a: list[bool], b:list[bool], n=8):
+
+        if n <= 1:
+            raise ValueError("n cannot be less than or equal to one")
+        
+        if isinstance(a, list) == False or isinstance(b, list)  == False:
+            raise ValueError("Inputs should be a list")
+        
+        if len(a) < n or len(b) < n:
+            raise ValueError("Input list cannot be less than n")
+
+        full_result = []
+        
+        result = HalfSubstractor.logic(a[n-1], b[n-1])
+        
+        full_result.append(result["sub"])
+        for i in reversed(range(n-1)):
+            result = FullSubstractor.logic(a[i], b[i], result["remainder"])
+            full_result.append(result["sub"])
+
+        result = int(bool(result["remainder"]))
+        
+        full_result.append(result)
+        full_result.reverse()
+        return full_result
+    
+    def __call__(self, a, b, n):
+        A = a if a != None else self.A
+        B = b if b != None else self.B
+        return self.logic(A, B, n)
 
 
 
